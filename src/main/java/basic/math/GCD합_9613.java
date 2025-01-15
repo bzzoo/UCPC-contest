d18af4f14521bbe5326123f42dc0c383a2ebc848
bzzoo <fullbster@gmail.com>
2025-01-15 22:46:18 +0900

    GCD: 9613, 17087

---
+++ src/main/java/basic/math/GCD합_9613.java
@@ -0,0 +1,34 @@
+package basic.math;
+
+import java.util.Scanner;
+
+public class GCD합_9613 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            long sum = 0;
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    sum += gcd(arr[i], arr[j]);
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+
+    static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
---
+++ src/main/java/basic/math/숨바꼭질6_17087.java
@@ -0,0 +1,33 @@
+package basic.math;
+
+import java.util.Scanner;
+
+public class 숨바꼭질6_17087 {
+
+    static int N, S, D;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        S = sc.nextInt();
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Math.abs(S - sc.nextInt());
+        }
+
+        D = arr[0];
+        for (int i = 1; i < N; i++) {
+            D = gcd(D, arr[i]);
+        }
+        System.out.println(D);
+    }
+
+    static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+}
