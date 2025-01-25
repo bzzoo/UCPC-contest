13fb67fb5f0a0e6c0f7042de4839a1b67478583f
bzzoo <fullbster@gmail.com>
2025-01-25 21:42:30 +0900

    MUTATION : 10972, 10973, 10974, 10819

---
+++ src/main/java/basic/permutation/다음순열_10972.java
@@ -0,0 +1,59 @@
+package basic.permutation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 다음순열_10972 {
+
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        if (nextPermutation(arr)) {
+            for (int i = 0; i < N; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        } else {
+            System.out.println(-1);
+        }
+    }
+
+    static boolean nextPermutation(int[] arr) {
+        int i = N - 1;
+        while (i > 0 && arr[i - 1] >= arr[i]) {
+            i--;
+        }
+        if (i <= 0) {
+            return false;
+        }
+
+        int j = N - 1;
+        while (arr[i - 1] >= arr[j]) {
+            j--;
+        }
+
+        int temp = arr[i - 1];
+        arr[i - 1] = arr[j];
+        arr[j] = temp;
+
+        j = N - 1;
+        while (i < j) {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
---
+++ src/main/java/basic/permutation/모든순열_10974.java
@@ -0,0 +1,36 @@
+package basic.permutation;
+
+import java.util.Scanner;
+
+public class 모든순열_10974 {
+
+    static int N;
+    static int[]arr;
+    static boolean[] visited;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        visited = new boolean[N];
+        arr = new int[N];
+        rec(0);
+    }
+
+    static void rec(int depth) {
+        if (depth == N) {
+            for (int i = 0; i < N; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = 1; i<=N; i++){
+            if(visited[i-1]) continue;
+            visited[i-1] = true;
+            arr[depth] = i;
+            rec(depth+1);
+            visited[i-1] = false;
+        }
+    }
+}
